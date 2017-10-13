58d1eac690e9c98fa085abc53969dce17a088be7
Diego Murillo <ddmurillo@MBP-de-Diego.home>
2017-10-12 23:07:37 -0500

    Actualizacion a ImageBackground en lugar de Image

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, ImageBackground } from 'react-native';
 
 import styles from './styles';
 
 const Logo = () => (
   <View style={styles.container}>
-    <Image
+    <ImageBackground
       source={require('./images/background.png')}
       style={styles.containerImage}
       resizeMode="contain"
     >
       <Image source={require('./images/logo.png')} style={styles.image} resizeMode="contain" />
-    </Image>
+    </ImageBackground>
     <Text style={styles.text}>Currency 2</Text>
   </View>
 );
